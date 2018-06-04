6f66e5719f842b658b1ef78a7ebdc5b0e52ac60e
abhidarbey <abhi.darbey@gmail.com>
2018-06-04 16:17:02 +0530

    keys update

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
-  secretOrKey: 'secret'
+  mongoURI: 'mongodb://tiger:tiger123@ds139970.mlab.com:39970/hummingbird',
+  secretOrKey: '@tiger'
 };
